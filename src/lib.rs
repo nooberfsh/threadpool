cede9eae12736046aa54e5970aa84b52bc308a7c
nooberfsh <nooberfsh@gmail.com>
2017-07-10 00:29:45 -0700

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,11 @@ impl<T: Runable> ThreadPool<T> {
             .send(self.tasks.clone())
             .unwrap();
     }
+
+    pub fn waiting_tasks_num(&self) -> usize {
+        let lock = self.tasks.lock().unwrap();
+        lock.len()
+    }
 }
 
 impl<T: Runable> Drop for ThreadPool<T> {
@@ -77,5 +82,125 @@ impl<T: Runable> Drop for ThreadPool<T> {
         while let Some(w) = self.workers.pop() {
             w.join().unwrap();
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::sync::atomic::{self, AtomicUsize};
+    use std::cmp;
+    use std::thread;
+
+    struct TaskA<F: FnOnce() + Send + 'static> {
+        pri: usize,
+        run_count: Arc<AtomicUsize>,
+        abandon_fn: Mutex<Option<F>>,
+    }
+
+    impl<F> TaskA<F>
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        fn new(pri: usize, run_count: Arc<AtomicUsize>) -> Self {
+            TaskA {
+                pri: pri,
+                run_count: run_count,
+                abandon_fn: Default::default(),
+            }
+        }
+
+        fn register_abandon(&self, f: F) {
+            let mut lock = self.abandon_fn.lock().unwrap();
+            *lock = Some(f);
+        }
+    }
+
+    impl<F> Ord for TaskA<F>
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        fn cmp(&self, other: &Self) -> cmp::Ordering {
+            self.pri.cmp(&other.pri)
+        }
+    }
+
+    impl<F> PartialOrd for TaskA<F>
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    impl<F> Eq for TaskA<F>
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+
+    impl<F> PartialEq for TaskA<F>
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        fn eq(&self, other: &Self) -> bool {
+            self.pri == other.pri
+        }
+    }
+
+    impl<F> Runable for TaskA<F>
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        fn run(&self) {
+            self.run_count.fetch_add(1, atomic::Ordering::SeqCst);
+        }
+
+        fn abandon(&self) {
+            self.abandon_fn.lock().unwrap().take().map(|t| t());
+        }
+    }
+
+    #[test]
+    fn test_new() {
+        let run_count = Arc::new(AtomicUsize::new(0));
+        let task_num = 1000_usize;
+        {
+            let tp = ThreadPool::new(1);
+            for i in 0..task_num {
+                let t = Arc::new(TaskA::new(i, run_count.clone()));
+                t.register_abandon(|| {});
+                tp.accept(t)
+            }
+            loop {
+                if tp.waiting_tasks_num() == 0 {
+                    break;
+                }
+                thread::sleep(std::time::Duration::from_secs(1));
+            }
+        }
+
+        assert_eq!(task_num, run_count.load(atomic::Ordering::SeqCst));
+    }
+
+    #[test]
+    fn test_drop() {
+        let run_count = Arc::new(AtomicUsize::new(0));
+        let abandon_count = Arc::new(AtomicUsize::new(0));
+        let task_num = 1 << 20;
+        {
+            let tp = ThreadPool::new(1);
+            for i in 0..task_num {
+                let t = Arc::new(TaskA::new(i, run_count.clone()));
+                let a = abandon_count.clone();
+                t.register_abandon(move || { a.fetch_add(1, atomic::Ordering::SeqCst); });
+                tp.accept(t)
+            }
+        }
+        let num = run_count.load(atomic::Ordering::SeqCst) +
+            abandon_count.load(atomic::Ordering::SeqCst);
+        assert_eq!(num, task_num);
     }
 }
